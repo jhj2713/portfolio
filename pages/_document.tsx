47d6361d4d0a39c5c6bba6ef014568cec3cd80fd
juhyojeong <juhyojeong58@gmail.com>
2023-02-10 10:32:45 +0900

    fix: og image 수정

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <link rel="icon" href="/icon.ico" />
         <meta property="og:title" content="주효정 | 포트폴리오" />
-        <meta property="og:image" content="/image/og-image.png" />
+        <meta property="og:image" content="/og-image.png" />
         <meta property="og:description" content="프론트엔드 개발자의 포트폴리오입니다." />
       </Head>
       <body>
